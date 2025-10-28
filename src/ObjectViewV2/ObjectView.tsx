c57f2b8c6e58939049fa9b50a976581ee8ee30d7
Dat Vo <vothanhdat@outlook.com>
2025-10-28 19:04:58 +0700

    🔧 Integrate GroupedProxy and enhance object rendering with new styles

--- src/ObjectViewV2/ObjectView.tsx
+++ src/ObjectViewV2/ObjectView.tsx
@@ -1,5 +1,7 @@
-import React, { useCallback, useDeferredValue, useEffect, useEffectEvent, useMemo, useRef, useState } from "react";
-import { createIterator } from "./utils";
+import React, { CSSProperties, Fragment, useCallback, useDeferredValue, useEffect, useEffectEvent, useMemo, useRef, useState } from "react";
+import "./style.css";
+import { GroupedProxy, getObjectGroupProxy } from "./GroupedProxy";
+
 
 export type Constructor<T = {}> = new (...args: any[]) => T;
 
@@ -12,10 +14,18 @@ type JSONViewCtx = {
 
 const useValueInfo = (value: any, path: string, level: number, trace: any[], isNonenumerable: boolean, { expandLevel, expandRef }: JSONViewCtx) => {
     const [, reload] = useState(0)
-    const hasChilds = value && value instanceof Object && !(value instanceof Date) && !(value instanceof RegExp)
-    const defaultExpand = hasChilds && !isNonenumerable && level <= expandLevel;
+
+    const isInGroupping = value instanceof GroupedProxy
+
+    const hasChilds = value && (
+        (value instanceof Object && !(value instanceof Date) && !(value instanceof RegExp))
+        || value == Object.prototype
+    )
+
+    const defaultExpand = hasChilds && !isNonenumerable && level < expandLevel;
+
     const expandChild = hasChilds && (expandRef.current[path] ?? defaultExpand)
-    // console.log({ path, "expandRef.current[path]": expandRef.current[path] })
+
     const setExpandChild = useCallback(
         (value: boolean) => {
             expandRef.current[path] = value;
@@ -28,6 +38,7 @@ const useValueInfo = (value: any, path: string, level: number, trace: any[], isN
         hasChilds,
         expandChild,
         setExpandChild: hasChilds ? setExpandChild : undefined,
+        isInGroupping,
     }
 }
 
@@ -41,81 +52,102 @@ type ObjectRenderProps = {
     renderName?: boolean
 }
 
+const joinClasses = (...e: (string | false | undefined)[]) => e.filter(Boolean).join(" ")
 
-const ValueInline: React.FC<{ value: any, isPreview: boolean }> = ({ value, isPreview = false }) => {
+
+const ValueInline: React.FC<{ value: any, isPreview: boolean, className?: string }> = ({ value, className, isPreview = false }) => {
     switch (typeof value) {
         case "boolean":
         case "number":
         case "symbol":
         case "undefined":
         case "function":
-            return <span className={"value type-" + typeof value}>{String(value)}</span>
+            return <span className={joinClasses("value", "type-" + typeof value, className)}>{String(value)}</span>
         case "bigint":
-            return <span className={"value type-" + typeof value}>{String(value)}n</span>
+            return <span className={joinClasses("value", "type-" + typeof value, className)}>{String(value)}n</span>
         case "string":
-            return <span className={"value type-" + typeof value}>{JSON.stringify(value)}</span>
+            return <span className={joinClasses("value", "type-" + typeof value, className)}>{JSON.stringify(value)}</span>
         case "object": {
+            const classes = joinClasses(`value type-object-${value?.constructor?.name?.toLowerCase() ?? "null"}`, className);
             if (!value) {
-                return <span className={"value type-null"}>{String(value)}</span>
+                return <span className={classes}>{String(value)}</span>
             } else if (value instanceof RegExp) {
-                return <span className={"value type-regexp"}>{String(value)}</span>
+                return <span className={classes}>{String(value)}</span>
             } else if (value instanceof Date) {
-                return <span className={"value type-date"}>{String(value)}</span>
+                return <span className={classes}>{String(value)}</span>
             } else if (value instanceof Error) {
-                return <span className={"value type-error"}>{String(value)}</span>
+                return <span className={joinClasses("value", "type-object-error", className)}>{String(value)}</span>
             } else if (value instanceof Array) {
                 return isPreview
-                    ? <span>{`Array(${value.length})`}</span>
-                    : <AllChildsPreview value={value} />
+                    ? <span className={classes} >{`Array(${value.length})`}</span>
+                    : <AllChildsPreview className={joinClasses('value', className)} value={value} />
+            } else if (value instanceof Map || value instanceof Set) {
+
+                return isPreview
+                    ? <span className={classes} >{`${value.constructor?.name}(${value.size})`}</span>
+                    : <AllChildsPreview className={joinClasses('value', className)} value={value} />
             } else if (value instanceof Object) {
                 return isPreview
-                    ? <span>{(value.constructor?.name ?? `Object`)}</span>
-                    : <AllChildsPreview value={value} />
+                    ? <span className={classes}>{(value.constructor?.name ?? `Object`)}</span>
+                    : <AllChildsPreview className={joinClasses('value', className)} value={value} />
             }
         }
     }
     return <span>{JSON.stringify(value, null, 2)}</span>
 }
 
-const AllChildsPreview: React.FC<{ value: any }> = ({ value }) => {
+const AllChildsPreview: React.FC<{ value: any, style?: React.CSSProperties, className?: string }> = ({ value, style, className }) => {
+
+    const allIterators = useMemo(
+        () => Object.entries(value)
+            .slice(0, 6)
+            .map(([name, data]) => ({ name, data, isNonenumerable: false })),
+        [value]
+    )
+
     const renderName = !(value instanceof Array || value instanceof Set)
-    const entries = Object.entries(value)
-    return <span style={{ opacity: 0.5, overflow: "hidden" }}>
+
+    const renderType = value
+        && value.constructor != Object
+        && value.constructor != Array
+        ? value.constructor?.name : ""
+
+    return <span className={joinClasses(className,)}>
+        {renderType}
         {value instanceof Array ? "[" : "{"}
-        {entries
+        {allIterators
             .slice(0, 5)
-            .map(([k, v], index) => <>
-                {index > 0 ? "," : ""}
-                {renderName && <><NameRender name={k} />:</>}
-                <ValueInline value={v} isPreview />
-            </>)}
-        {entries.length > 5 ? ",…" : ""}
+            .map(({ name, data, isNonenumerable }, index) => <Fragment key={name}>
+                {index > 0 ? ", " : ""}
+                {renderName && <><NameRender name={name} />: </>}
+                <ValueInline value={data} isPreview />
+            </Fragment>)}
+        {allIterators.length > 5 ? ",…" : ""}
         {value instanceof Array ? "]" : "}"}
     </span>
 }
 
-
 const AllChilds: React.FC<ObjectRenderProps> = ({ name, value, path = "", level = 0, context }) => {
-    // const renderName = !( value instanceof Set)
-    const iterator = useMemo(
-        () => createIterator(true, false),
-        []
+
+    const allKeys = useMemo(
+        () => Object.keys(value),
+        [value]
+    )
+    const renderObject = useMemo(
+        () => value instanceof GroupedProxy ? value
+            : allKeys.length >= 10 ? getObjectGroupProxy(value, allKeys, 10)
+                : value,
+        [value, allKeys]
     )
 
-    const allIterators = useMemo(
-        () => value ? [...iterator(value)] : [],
-        [iterator, value]
-    )
-
-
-    return allIterators
-        .map(({ name, data, isNonenumerable = false }) => <ObjectRender
-            key={path + "." + name}
+    return Object.entries(renderObject)
+        .map(([name, data]) => <ObjectRender
+            key={path + "." + String(name)}
             {...{
-                name: name,
+                name,
                 value: data,
-                isNonenumerable,
-                path: path + "." + name,
+                isNonenumerable: false,
+                path: path + "." + String(name),
                 level: level + 1,
                 context
             }}
@@ -123,25 +155,28 @@ const AllChilds: React.FC<ObjectRenderProps> = ({ name, value, path = "", level 
 }
 
 
-
-const NameRender: React.FC<{ name: string, isNonenumerable?: boolean }> = ({ name, isNonenumerable = false }) => {
-    return <span style={{ opacity: isNonenumerable ? 0.5 : 1 }}>{name}</span>
+const NameRender: React.FC<{ name: string }> = ({ name }) => {
+    return <span className="name">{String(name)}</span>
 }
 
-const ObjectRender: React.FC<ObjectRenderProps> = ({ name, value, path = "", level = 0, context, isNonenumerable, renderName = true, }) => {
-    const { expandChild, setExpandChild, hasChilds } = useValueInfo(value, path, level, [], isNonenumerable, context)
 
+const ObjectRender: React.FC<ObjectRenderProps> = ({ name, value, path = "", level = 0, context, isNonenumerable, renderName = true, }) => {
+    const { expandChild, setExpandChild, hasChilds, isInGroupping } = useValueInfo(value, path, level, [], isNonenumerable, context)
+    const isPreview = expandChild || !context.preview
     return <>
-        <div onClick={() => setExpandChild?.(!expandChild)} style={{
-            overflow: "hidden",
-            whiteSpace: "preserve nowrap",
-            textOverflow: "ellipsis"
-        }}>
-            <span style={{ opacity: 0.5 }}>{hasChilds ? (expandChild ? "▼ " : "▶ ") : "  "}</span>
-            {renderName && <><NameRender {...{ name, isNonenumerable }} />:</>}
-            <ValueInline {...{ value, isPreview: expandChild || !context.preview }} />
+        <div onClick={() => setExpandChild?.(!expandChild)}
+            className={joinClasses("node-default", isNonenumerable && "non-enumrable")}
+            style={{ whiteSpace: expandChild ? "preserve nowrap" : "nowrap", }}
+        >
+            <span className="expand-symbol">{hasChilds ? (expandChild ? "▼ " : "▶ ") : "  "}</span>
+            {renderName && <><NameRender {...{ name }} />: </>}
+            {!isInGroupping && <ValueInline {...{
+                value,
+                isPreview,
+                className: hasChilds ? "value-preview" : "",
+            }} />}
         </div>
-        {expandChild ? <div style={{ paddingLeft: "1.2em" }}>
+        {expandChild ? <div className="node-child">
             <AllChilds {...{ name, value, path, level, context, isNonenumerable }} />
         </div> : ""}
 
@@ -162,7 +197,7 @@ export type ObjectViewProps = {
 
 export const ObjectViewV2: React.FC<ObjectViewProps> = ({
     value, name = "", style, expandLevel = false,
-    objectGrouped = 25, arrayGrouped = 10,
+    objectGrouped = 100, arrayGrouped = 10,
     customRender = undefined,
     highlightUpdate = true
 }) => {
@@ -175,11 +210,8 @@ export const ObjectViewV2: React.FC<ObjectViewProps> = ({
         preview: true,
     } as JSONViewCtx), [expandLevel, expandRootRef])
 
-
-    return <div className="jv-root" style={style}>
-        <pre style={{ padding: 0 }}>
-            <ObjectRender {...{ name, value, context, level: 0, path: "" }} />
-        </pre>
+    return <div className="objview-root" style={style}>
+        <ObjectRender {...{ name, value, context, level: 0, path: "", isNonenumerable: false }} />
     </div>
 };
 
