51782ff1fdf74da662941d0982e7da1713a3b4b8
Dat Vo <vothanhdat@outlook.com>
2025-11-03 17:58:26 +0700

    🔧 expose getState without clean memory for flexible use

--- src/utils/memorizeMapWithWithClean.ts
+++ src/utils/memorizeMapWithWithClean.ts
@@ -8,78 +8,93 @@ export const memorizeMapWithWithClean = <T extends (...param: any[]) => any>(
     const resultSymbol = Symbol("result")
 
 
-    return (...params: any[]) => {
+    return {
+        stateFactory: (...params: any[]) => {
+            let current: Map<any, any> = rootMap,
+                parent: Map<any, any> = rootMap,
+                lastParam: any = undefined,
+                cleaned = false;
 
-        let current: Map<any, any> = rootMap,
-            parent: Map<any, any> = rootMap,
-            lastParam: any = undefined,
-            cleaned = false;
+            const touchedKeys = new Set<any>();
+            let hasNewKey = false;
 
-        const touchedKeys = new Set<any>();
-        let hasNewKey = false;
+            for (let param of ['@', ...params]) {
+                parent = current;
+                lastParam = param;
 
-        for (let param of ['@', ...params]) {
-            parent = current;
-            lastParam = param;
-            
-            if (!current.has(param))
-                current.set(param, new Map<any, any>());
-            current = current.get(param)
-        }
+                if (!current.has(param))
+                    current.set(param, new Map<any, any>());
+                current = current.get(param)
+            }
 
-        if (!current || (parent == current)) {
-            throw new Error("memorizeMapWithWithClean unexpected error")
-        }
+            if (!current || (parent == current)) {
+                throw new Error("memorizeMapWithWithClean unexpected error")
+            }
 
-        return {
-            get(key: any) {
-                if (cleaned) {
-                    throw new Error("memorizeMapWithWithClean cleaned")
-                }
+            return {
+                get(key: any) {
+                    if (cleaned) {
+                        throw new Error("memorizeMapWithWithClean cleaned")
+                    }
 
-                touchedKeys.add(key);
+                    touchedKeys.add(key);
 
-                let currentMap = current.get(key);
+                    let currentMap = current.get(key);
 
-                if (!currentMap) {
-                    currentMap = new Map<any, any>();
-                    current.set(key, currentMap);
-                    hasNewKey = true;
-                }
+                    if (!currentMap) {
+                        currentMap = new Map<any, any>();
+                        current.set(key, currentMap);
+                        hasNewKey = true;
+                    }
 
-                //@ts-ignore
-                return (currentMap[resultSymbol] ||= onNewEl(...params, key))
+                    //@ts-ignore
+                    return (currentMap[resultSymbol] ||= onNewEl(...params, key))
 
-            },
-            clean() {
-                if (cleaned) {
-                    return;
-                }
-                let deleted = 0;
+                },
+                clean() {
+                    if (cleaned) {
+                        return;
+                    }
+                    let deleted = 0;
 
-                if (!hasNewKey && touchedKeys.size === current.size) {
-                    // fully reused, no pruning needed
-                } else {
-                    for (const key of current.keys()) {
-                        if (!touchedKeys.has(key)) {
-                            current.delete(key);
-                            deleted++;
+                    if (!hasNewKey && touchedKeys.size === current.size) {
+                        // fully reused, no pruning needed
+                    } else {
+                        for (const key of current.keys()) {
+                            if (!touchedKeys.has(key)) {
+                                current.delete(key);
+                                deleted++;
+                            }
                         }
                     }
+
+                    deleted >= 100 && console.log("CLEAN delete %s current size %s", deleted, current.size);
+
+                    if (current.size === 0 && !Object.prototype.hasOwnProperty.call(current, resultSymbol)) {
+                        parent.delete(lastParam);
+                    }
+
+                    cleaned = true;
+                    //@ts-ignore
+                    current = null;
+                    //@ts-ignore
+                    parent = null;
                 }
-
-                deleted >= 100 && console.log("CLEAN delete %s current size %s", deleted, current.size);
-
-                if (current.size === 0 && !Object.prototype.hasOwnProperty.call(current, resultSymbol)) {
-                    parent.delete(lastParam);
-                }
-
-                cleaned = true;
-                //@ts-ignore
-                current = null;
-                //@ts-ignore
-                parent = null;
             }
+        },
+        getState: (...paths: any) => {
+            let current: Map<any, any> = rootMap;
+            // console.log(['@', ...paths])
+            for (let path of ['@', ...paths]) {
+                current = current.get(path)
+                if (!current) {
+                    throw new Error("Get State Error")
+                }
+            }
+
+            //@ts-ignore
+            return current[resultSymbol]
+
         }
     }
 }
